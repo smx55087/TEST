5fa0b9bb3d5be3d77214828e22207fe6bac40ec2
smx55087 <31880229+smx55087@users.noreply.github.com>
2018-01-03 15:55:58 +0800

    Update Complex.java

--- Homework2/src/Complex.java
+++ Homework2/src/Complex.java
@@ -1,61 +1,45 @@
 public class Complex 
 {
 	public double r,x;
-	public Complex(	double r,double x) {this.r=r;this.x=x;}
-	public void Add(Complex a)
-	{		
+	public Complex(	String r,String x) 
+	{
 		try 
 		{
-			this.r=a.r+this.r;
-			this.x=a.x+this.x;
+			this.r=Double.parseDouble(r);
+			this.x=Double.parseDouble(x);
 		}
 		catch(NumberFormatException ex)
 		{
 			new MessageJDialog().showing2(ex);
+			return;
 		}
+	}
+	public void Add(Complex a)
+	{		
+		this.r=a.r+this.r;
+		this.x=a.x+this.x;
 	}
 	public void Subtract(Complex a)
 	{	
-		try 
-		{
-			this.r=this.r-a.r;
-			this.x=this.x-a.x;
-		}
-		catch(NumberFormatException ex)
-		{
-			new MessageJDialog().showing2(ex);
-		}
+		this.r=this.r-a.r;
+		this.x=this.x-a.x;
 	}
 	public void Multiply(Complex a)
 	{	
-		Complex c2=new Complex(0,0);
+		Complex c2=new Complex("0","0");
 		c2.r=this.r;
 		c2.x=this.x;
-		try 
-		{
-			this.r=c2.r*a.r-c2.x*a.x;
-			this.x=c2.r*a.x+c2.x*a.r;
-		}
-		catch(NumberFormatException ex)
-		{
-			new MessageJDialog().showing2(ex);
-		}
+		this.r=c2.r*a.r-c2.x*a.x;
+		this.x=c2.r*a.x+c2.x*a.r;
 	}
 	public void Divided(Complex a)
 	{	
-		Complex c2=new Complex(0,0);
+		Complex c2=new Complex("0","0");
 		c2.r=this.r;
 		c2.x=this.x;
 		if(a.r==0&&a.x==0)
 			new MessageJDialog().showing();
-		try 
-		{
-			this.r=(c2.r*a.r+c2.x*a.x)/(a.r*a.r+a.x*a.x);
-			this.x=(c2.x*a.r-c2.r*a.x)/(a.r*a.r+a.x*a.x);
-		}
-		catch(NumberFormatException ex)
-		{
-			new MessageJDialog().showing2(ex);
-		}
+		this.r=(c2.r*a.r+c2.x*a.x)/(a.r*a.r+a.x*a.x);
+		this.x=(c2.x*a.r-c2.r*a.x)/(a.r*a.r+a.x*a.x);
 	}
 }
